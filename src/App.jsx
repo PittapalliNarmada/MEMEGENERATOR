e07573ca0b9f522179f1586587e5a7e89c24ac42
narmadap <sumap887@gmail.com>
2026-02-17 21:24:00 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
